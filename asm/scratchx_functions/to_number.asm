65616556e6f1bf21246ed5c382c9fb88e2efd5f8
Alex Curran <haletas88@gmail.com>
2026-05-26 22:20:05 +1200

    fix: add comments to asm functions

--- asm/scratchx_functions/to_number.asm
+++ asm/scratchx_functions/to_number.asm
@@ -14,20 +14,24 @@ to_number:
     ret
 
 small_string:
-    sub rsp, 264
-    mov rcx, rsi
-    mov rdx, rcx
+    sub rsp, 264 ; Create a buffer for the null-terminated string 256 bytes + alignment
 
+    mov rcx, rsi
+    mov rdx, rcx ; Save length
+
+    ; Fill the buffer
     mov rsi, rdi
     mov rdi, rsp
     rep movsb
 
-    mov byte [rsp + rdx], 0
+    mov byte [rsp + rdx], 0 ; Null-terminate
 
+    ; Get the double version of the string
     mov rdi, rsp
     xor esi, esi
     call strtod
 
+    ; Cleanup
     add rsp, 264
     ret
 
@@ -36,30 +40,35 @@ heap_string:
     push r13
     push r14
 
-    mov r12, rsi
-    mov r13, rdi
+    mov r12, rsi ; Save length
+    mov r13, rdi ; Save data
 
+    ; Create the buffer for the null-terminated string
     inc rsi
     mov rdi, rsi
 
     call malloc
 
-    mov r14, rax
+    mov r14, rax ; Save the malloced buffer
     mov rdi, rax
 
+    ; Move the string into the buffer
     mov rcx, r12
     mov rsi, r13
     rep movsb
 
-    mov byte [rdi], 0
-    mov rdi, r14
-    xor esi, esi
+    mov byte [rdi], 0 ; Null-terminate
+
+    ; Get the double version of the string
+    mov rdi, r14 ; string
+    xor esi, esi ; Endptr
     call strtod
 
     sub rsp, 16
-    movsd [rsp], xmm0
-
+    movsd [rsp], xmm0 ; Save xmm0 from being clobbered by free
     mov rdi, r14
+
+    ; Clean up
     call free
 
     movsd xmm0, [rsp]
--- asm/scratchx_functions/to_string.asm
+++ asm/scratchx_functions/to_string.asm
@@ -12,12 +12,14 @@ to_string:
     push r13
     sub rsp, 8
 
-    movq r12, xmm0
+    movq r12, xmm0 ; Save the double
 
+    ; Allocate a 24 byte buffer for the string version of the double
     mov rdi, 24
     call malloc
-    mov r13, rax
+    mov r13, rax ; Save the buffer
 
+    ; Gete the string version of the double
     mov rdi, r13
     mov rsi, 24
     lea rdx, [rel format]
@@ -26,9 +28,11 @@ to_string:
     mov al, 1
     call snprintf
 
-    mov rdx, rax
-    mov rax, r13
+    ; Prepare return values
+    mov rdx, rax ; length
+    mov rax, r13 ; string buffer
 
+    ; Cleanup
     add rsp, 8
     pop r13
     pop r12
